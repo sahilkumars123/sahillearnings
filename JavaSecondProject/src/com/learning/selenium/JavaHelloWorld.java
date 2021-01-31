30f34db8510ff978d1d532c05732d60f7188f142
Sagar Kumar <Sagar Kumar@LAPTOP-BPH16UFS>
2021-02-01 00:35:00 +0530

    Test

--- JavaSecondProject/src/com/learning/selenium/JavaHelloWorld.java
+++ JavaSecondProject/src/com/learning/selenium/JavaHelloWorld.java
@@ -4,7 +4,7 @@ public class JavaHelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Worlds");
+		System.out.println("Hello World");
 	}
 
 }
